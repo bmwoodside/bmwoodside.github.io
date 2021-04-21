4d9e6df9dde601d0f8c5add2dacaf8fb2d261cbf
Matt <bmwoodside@live.com>
2021-04-21 17:52:57 -0400

    updated DBConnect python file

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import pymongo
 
 client = pymongo.MongoClient("mongodb://dbAdmin:<password>@cluster0-shard-00-00.b00ti.mongodb.net:27017,cluster0-shard-00-01.b00ti.mongodb.net:27017,cluster0-shard-00-02.b00ti.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mydz1b-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.test
