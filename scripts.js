793c7f512412f499794ed38108cc7d9c75da664d
Matt Woodside <bmwoodside@live.com>
2021-05-02 20:12:33 -0400

    fixed the cards. Still fighting CSS >:(

--- scripts.js
+++ scripts.js
@@ -2,17 +2,10 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function navButton() {
     const x = document.getElementById("myLinks");
-    const y = document.getElementById("navbar");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
 
-    if (x.style.display === "none") {
-      y.style.border = "none";
-    } else {
-      y.style.border = "1px solid honeydew";
-    }
-    
   }
