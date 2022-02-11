d5fc0feda96143cb5bcc6fe7cbf1e3019d93cb6b
Matt Woodside <bmwoodside@live.com>
2022-02-11 00:22:09 -0500

    not finished yet, but have to restart computer and don't want to lose work.

--- db.py
+++
@@ -1,4 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb://dbAdmin:<password>@cluster0-shard-00-00.b00ti.mongodb.net:27017,cluster0-shard-00-01.b00ti.mongodb.net:27017,cluster0-shard-00-02.b00ti.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mydz1b-shard-0&authSource=admin&retryWrites=true&w=majority")
-db = client.test
